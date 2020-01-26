23477896da552a63f0b225484ef0e69589380885
jaanuser <374519711@qq.com>
2020-01-26 23:23:22 +0800

    nio_chat第一次提交

---
+++ src/main/java/nio_chat/client/UserInputHandler.java
@@ -0,0 +1,46 @@
+package nio_chat.client;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UserInputHandler implements Runnable {
+
+    //客户端
+    private ChatClient chatClient;
+
+    public UserInputHandler(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @Override
+    public void run() {
+        BufferedReader reader = null;
+        try {
+            //获取本地输入
+            reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+            //循坏等待用户输入
+            while (true) {
+                String msg = reader.readLine();
+
+                chatClient.send(msg);
+                //判断是否退出
+                if (chatClient.readToQuit(msg)) {
+                    break;
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/nio_chat/server/ChatServer.java
@@ -0,0 +1,126 @@
+package nio_chat.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Set;
+
+public class ChatServer {
+    private  final int PORT = 8888;
+    private  ByteBuffer rBuffer;
+    private  ByteBuffer wBuffer;
+    private  final int BUFFER_SIZE = 1024;
+    private  final String QUIT = "qiut";
+
+    public void star() {
+        try {
+
+            //启动服务器
+            ServerSocketChannel server = ServerSocketChannel.open();
+            //默认是阻塞式的调用
+            server.configureBlocking(false);
+            server.socket().bind(new InetSocketAddress(PORT));
+            System.out.println("启动服务器,监听端口" + server.socket().getLocalPort());
+
+
+            rBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+            wBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+
+            Selector selector = Selector.open();
+            //注册事件
+            server.register(selector, SelectionKey.OP_ACCEPT);
+
+            //轮询处理事件//select()返回触发的事件个数
+            while (true) {
+                selector.select();
+                System.out.println("已有事件被触发");
+                //得到触发的事件key
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                for (SelectionKey key : selectionKeys) {
+                    //处理事件
+                    handles(key);
+                }
+                //手动清空处理多的key,如果不clear的话,处理过的key还在里面
+                selectionKeys.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 处理请求
+    private void handles(SelectionKey key) throws IOException {
+
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+        // 处理ACCEPT事件
+        if (key.isAcceptable()) {
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            //默认为阻塞式的
+            socketChannel.configureBlocking(false);
+            socketChannel.register(key.selector(), SelectionKey.OP_READ);
+            System.out.println("客户端:" + socketChannel.socket().getPort() + "已连接");
+
+            // 处理READ事件
+        } else if (key.isReadable()) {
+            //读取消息
+            String msg = readMsg((SocketChannel) key.channel());
+            // 客户端异常
+            if (msg.isEmpty()) {
+                key.cancel();
+                key.selector().wakeup();
+            } else {
+                //转发消息
+                forwardMsg((SocketChannel) key.channel(),key, msg);
+                //判断是否要退出
+                if (QUIT.equals(msg)) {
+                    key.cancel();
+                    key.selector().wakeup();
+                    System.out.println("客户端" + ((SocketChannel) key.channel()).socket().getPort() + "已退出");
+                }
+            }
+
+        }
+    }
+
+    // 读消息
+    private String readMsg(SocketChannel channel) throws IOException {
+
+        rBuffer.clear();
+        // 如果用户一直输入的话,这里就一直读取,因为只要不关channel,用户可能一直输入
+        while (channel.read(rBuffer) > 0) ;
+        rBuffer.flip();
+        System.out.println("客户端"+channel.socket().getPort()+rBuffer.toString());
+        return rBuffer.toString();
+
+    }
+
+    // 转发消息
+    private void forwardMsg(SocketChannel channel, SelectionKey key, String msg) throws IOException {
+        // 只要注册的key都显示
+        Set<SelectionKey> keys = key.selector().keys();
+        for (SelectionKey selectionKey : keys) {
+            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+            if (selectionKey.channel() instanceof ServerSocketChannel) continue;
+            //判断selector是否正常
+            if (key.isValid() && selectionKey.channel().equals(channel)) {
+                wBuffer.clear();
+                wBuffer.put(msg.getBytes());
+                wBuffer.flip();
+                while (wBuffer.hasRemaining()) {
+                    socketChannel.write(wBuffer);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+        chatServer.star();
+    }
+
+
+}
